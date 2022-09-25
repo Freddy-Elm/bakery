01097967af3e49e5f5cb59c0c906a3680c217834
Freddy-Elm <112373230+Freddy-Elm@users.noreply.github.com>
2022-09-25 11:17:33 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+$(function() {
+  $('#bread-show').click(function(){
+    $('#bread-modal').fadeIn();
+  });
+
+  $('#carry-show').click(function(){
+    $('#carry-modal').fadeIn();
+  });
+
+  $('#cream-show').click(function(){
+    $('#cream-modal').fadeIn();
+  });
+
+  $('#melon-show').click(function(){
+    $('#melon-modal').fadeIn();
+  });
+
+  $('#sand-show').click(function(){
+    $('#sand-modal').fadeIn();
+  });
+
+  $('#kurowassan-show').click(function(){
+    $('#kurowassan-modal').fadeIn();
+  });
+
+  $('#an-show').click(function(){
+    $('#an-modal').fadeIn();
+  });
+
+  $('#bageto-show').click(function(){
+    $('#bageto-modal').fadeIn();
+  });
+
+  $('#garik-show').click(function(){
+    $('#garik-modal').fadeIn();
+  });
+  
+  $('.close-modal').click(function(){
+    $('#bread-modal').fadeOut();
+    $('#carry-modal').fadeOut();
+    $('#cream-modal').fadeOut();
+    $('#melon-modal').fadeOut();
+    $('#sand-modal').fadeOut();
+    $('#kurowassan-modal').fadeOut();
+    $('#an-modal').fadeOut();
+    $('#bageto-modal').fadeOut();
+    $('#garik-modal').fadeOut();
+  });
+  
+  
+});
+
